aedb06cd952396cbdafafa2c300deb1068d76487
VarunKumar988 <cvarunkumar455@gmail.com>
2026-01-12 18:28:21 +0530

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-    buildActivityPosition: 'bottom-right',
+
+
   },
 };
 
